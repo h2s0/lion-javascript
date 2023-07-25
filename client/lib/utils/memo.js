94d31f26e8615def1f5ab749efd15fdb74ad3ae0
h2s0 <h2s0_@naver.com>
2023-07-25 09:19:09 +0900

    랜덤다이스 끝

--- client/lib/utils/memo.js
+++ client/lib/utils/memo.js
@@ -1,31 +1,25 @@
 import { getNode } from "../dom/getNode.js"
 
 
-const cache = {
+export const memo = (()=>{
 
-}
-
-export const memo = ( key,callback ) => {
+  const cache = {
+  
+  }
+  
+  return (key,callback)=>{
 
     if(!callback) return cache[key]
-
     if(cache[key]){
-        console.warn(`${key}는 이미 캐시된 값이 존재합니다.`);
-        return cache[key]
+      console.warn(`${key}는 이미 캐시된 값이 존재합니다.`);
+      return cache[key]
     }
-
     cache[key] = callback();
+  }
 
-    console.log(cache);    
+})()
 
-}
 
-// 메모라는 함수를 사용해 key 값을 전달된 뒤 그 안에서 
-memo('cube',()=> getNode('#cube'))
-memo('cube') // div#cube
+console.log( memo('cube') );
 
-// 객체를 만들어서 key:value 쌍으로 저장
-
-{
-    cube : <div id ="cube"></div>
-}
+// 객체를 만들어서 key:value 쌍으로 저장 
